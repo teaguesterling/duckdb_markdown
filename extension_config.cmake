5855630df1622f997ed8a6c498e7b86b2df21184
Teague Sterling <teaguesterling@gmail.com>
2026-01-11 22:37:15 -0800

    Add debug output to trace WASM LINKED_LIBS variable

--- extension_config.cmake
+++ extension_config.cmake
@@ -3,11 +3,14 @@
 # For WASM builds, we need EMULATE_FUNCTION_POINTER_CASTS=1 because cmark-gfm uses
 # function pointers for iterators and renderers that cause "indirect call signature mismatch"
 # errors without this flag. See: https://github.com/emscripten-core/emscripten/issues/5034
+message(STATUS "extension_config.cmake: EMSCRIPTEN=${EMSCRIPTEN}, CMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}")
 if(EMSCRIPTEN)
     set(MARKDOWN_WASM_LINKED_LIBS "-sEMULATE_FUNCTION_POINTER_CASTS=1")
     message(STATUS "WASM build: markdown extension will use EMULATE_FUNCTION_POINTER_CASTS for cmark-gfm")
+    message(STATUS "MARKDOWN_WASM_LINKED_LIBS=${MARKDOWN_WASM_LINKED_LIBS}")
 else()
     set(MARKDOWN_WASM_LINKED_LIBS "")
+    message(STATUS "Non-WASM build: EMSCRIPTEN not defined")
 endif()
 
 # Extension from this repo
@@ -16,6 +19,9 @@ duckdb_extension_load(markdown
     LOAD_TESTS
     LINKED_LIBS "${MARKDOWN_WASM_LINKED_LIBS}"
 )
+
+# Debug: verify the variable was set correctly after duckdb_extension_load
+message(STATUS "After duckdb_extension_load: DUCKDB_EXTENSION_MARKDOWN_LINKED_LIBS=${DUCKDB_EXTENSION_MARKDOWN_LINKED_LIBS}")
 
 # Any extra extensions that should be built
 # e.g.: duckdb_extension_load(json)
